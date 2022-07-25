eb0e26b6c7e56e98c176030000b16b3a1f39ac5c
Gabriel Cardoso <gabrielcardosodeveloper@gmail.com>
2022-07-25 11:15:31 -0300

    feat(Initial setup): :sparkles:

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { GlobalStyles } from './styles/global'
+
+export function App() {
+  return (
+    <>
+      <div className="App">
+        <h1>Hello World</h1>
+      </div>
+
+      <GlobalStyles />
+    </>
+  )
+}
---
+++ src/styles/themes/default.ts
@@ -0,0 +1,21 @@
+export const defaultTheme = {
+  white: '#FFF',
+
+  'gray-100': '#E1E1E6',
+  'gray-300': '#C4C4CC',
+  'gray-400': '#8D8D99',
+  'gray-500': '#7C7C8A',
+  'gray-600': '#323238',
+  'gray-700': '#29292E',
+  'gray-800': '#202024',
+  'gray-900': '#121214',
+
+  'brand-300': '#ECD6D9',
+  'brand-500': '#F3C2BD',
+  'brand-700': '#E38A8A',
+
+  'red-500': '#EE5959',
+  'red-700': '#7A1921',
+
+  'yellow-500': '#FBA94C',
+}
