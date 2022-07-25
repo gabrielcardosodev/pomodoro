24b16e72df86b85e513da52f507628b3bc85ed59
Gabriel Cardoso <gabrielcardosodeveloper@gmail.com>
2022-07-25 18:35:45 -0300

    refactor(Change archive name): :recycle:

---
+++ src/pages/History/index.tsx
@@ -0,0 +1,51 @@
+import { HistoryContainer, HistoryTable, Status } from './styles'
+
+export function History() {
+  return (
+    <HistoryContainer>
+      <h1>Meu histórico</h1>
+
+      <HistoryTable>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Inicio</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Ha 2 meses</td>
+              <td>
+                <Status statusColor={'finished'}>Concluída</Status>
+              </td>
+            </tr>
+
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Ha 2 meses</td>
+              <td>
+                <Status statusColor={'finished'}>Concluída</Status>
+              </td>
+            </tr>
+
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Ha 2 meses</td>
+              <td>
+                <Status statusColor={'finished'}>Concluída</Status>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryTable>
+    </HistoryContainer>
+  )
+}
