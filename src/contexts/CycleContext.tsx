a5540385e426d314acbcefeef1f62b07581b4db6
Gabriel Cardoso <gabrielcardosodeveloper@gmail.com>
2022-07-27 16:58:36 -0300

    feat(Add functionality to save a cycle in local storage): :sparkles:

--- src/contexts/CycleContext.tsx
+++ src/contexts/CycleContext.tsx
@@ -1,4 +1,10 @@
-import { createContext, ReactNode, useReducer } from 'react'
+import {
+  createContext,
+  ReactNode,
+  useEffect,
+  useReducer,
+  useState,
+} from 'react'
 import { Cycle, cycleReducer } from '../reducers/cycle/reducer'
 import {
   ActionTypes,
@@ -6,6 +12,7 @@ import {
   interruptCurrentCycleAction,
   markCurrentCycleAsFinishedAction,
 } from '../reducers/cycle/actions'
+import { differenceInSeconds } from 'date-fns'
 
 interface CreateCycleData {
   task: string
@@ -16,9 +23,11 @@ interface CycleContextType {
   cycles: Cycle[]
   activeCycle: Cycle | undefined
   activeCycleId: string | null
+  amountSecondsPassed: number
   markCycleCurrentAsFinished: () => void
   interruptCurrentCycle: () => void
   createNewCycle: (data: CreateCycleData) => void
+  setSecondsPassed: (seconds: number) => void
 }
 
 interface CycleContextProviderProps {
@@ -28,14 +37,38 @@ interface CycleContextProviderProps {
 export const CycleContext = createContext({} as CycleContextType)
 
 export function CycleContextProvider({ children }: CycleContextProviderProps) {
-  const [cyclesState, dispatch] = useReducer(cycleReducer, {
-    cycles: [],
-    activeCycleId: null,
-  })
+  const [cyclesState, dispatch] = useReducer(
+    cycleReducer,
+    {
+      cycles: [],
+      activeCycleId: null,
+    },
+    () => {
+      const storedStateAsJSON = localStorage.getItem('@pomodoro:cycles-state')
+
+      if (storedStateAsJSON) {
+        return JSON.parse(storedStateAsJSON)
+      }
+    },
+  )
 
   const { cycles, activeCycleId } = cyclesState
 
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
+
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState(() => {
+    if (activeCycle) {
+      return differenceInSeconds(new Date(), new Date(activeCycle.startDate))
+    }
+
+    return 0
+  })
+
+  useEffect(() => {
+    const stateJSON = JSON.stringify(cyclesState)
+
+    localStorage.setItem('@pomodoro:cycles-state', stateJSON)
+  }, [cyclesState])
 
   function createNewCycle(data: CreateCycleData) {
     const id = String(Math.random())
@@ -48,6 +81,10 @@ export function CycleContextProvider({ children }: CycleContextProviderProps) {
     }
 
     dispatch(addNewCycleAction(newCycle))
+  }
+
+  function setSecondsPassed(seconds: number) {
+    setAmountSecondsPassed(seconds)
   }
 
   function markCycleCurrentAsFinished() {
@@ -64,9 +101,11 @@ export function CycleContextProvider({ children }: CycleContextProviderProps) {
         cycles,
         activeCycle,
         activeCycleId,
+        amountSecondsPassed,
         markCycleCurrentAsFinished,
         interruptCurrentCycle,
         createNewCycle,
+        setSecondsPassed,
       }}
     >
       {children}
