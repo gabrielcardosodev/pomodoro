193ef05f0b1b011dd720d52c3c14c25857341b74
Gabriel Cardoso <gabrielcardosodeveloper@gmail.com>
2022-07-27 13:50:27 -0300

    feat(Create cycle context): :sparkles:
    This context is responsable for share to another components the cycle data

---
+++ src/contexts/CycleContext.tsx
@@ -0,0 +1,98 @@
+import { createContext, ReactNode, useState } from 'react'
+
+interface Cycle {
+  id: string
+  task: string
+  minutesAmount: number
+  startDate: Date
+  endDate?: Date
+  finishedDate?: Date
+}
+
+interface CreateCycleData {
+  task: string
+  minutesAmount: number
+}
+
+interface CycleContextType {
+  cycles: Cycle[]
+  activeCycle: Cycle | undefined
+  activeCycleId: string | null
+  markCycleCurrentAsFinished: () => void
+  interruptCurrentCycle: () => void
+  createNewCycle: (data: CreateCycleData) => void
+}
+
+interface CycleContextProviderProps {
+  children: ReactNode
+}
+
+export const CycleContext = createContext({} as CycleContextType)
+
+export function CycleContextProvider({ children }: CycleContextProviderProps) {
+  const [cycles, setCycles] = useState<Cycle[]>([])
+  const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
+
+  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
+
+  function createNewCycle(data: CreateCycleData) {
+    const id = String(Math.random())
+
+    const newCycle = {
+      id,
+      task: data.task,
+      minutesAmount: data.minutesAmount,
+      startDate: new Date(),
+    }
+
+    setCycles((state) => [...state, newCycle])
+    setActiveCycleId(id)
+  }
+
+  function markCycleCurrentAsFinished() {
+    setCycles((state) =>
+      state.map((cycle) => {
+        if (cycle.id === activeCycleId) {
+          return {
+            ...cycle,
+            finishedDate: new Date(),
+          }
+        }
+
+        return cycle
+      }),
+    )
+  }
+
+  function interruptCurrentCycle() {
+    setCycles((state) =>
+      state.map((cycle) => {
+        if (cycle.id === activeCycleId) {
+          return {
+            ...cycle,
+            endDate: new Date(),
+          }
+        }
+
+        return cycle
+      }),
+    )
+
+    setActiveCycleId(null)
+  }
+
+  return (
+    <CycleContext.Provider
+      value={{
+        cycles,
+        activeCycle,
+        activeCycleId,
+        markCycleCurrentAsFinished,
+        interruptCurrentCycle,
+        createNewCycle,
+      }}
+    >
+      {children}
+    </CycleContext.Provider>
+  )
+}
