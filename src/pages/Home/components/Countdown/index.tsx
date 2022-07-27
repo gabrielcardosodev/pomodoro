c4c9569996f0a2d58d26c5983a72f5af927d1fd5
Gabriel Cardoso <gabrielcardosodeveloper@gmail.com>
2022-07-27 16:57:54 -0300

    refactor(Format startDate to Date type): :recycle:

--- src/pages/Home/components/Countdown/index.tsx
+++ src/pages/Home/components/Countdown/index.tsx
@@ -5,9 +5,13 @@ import { CycleContext } from '../../../../contexts/CycleContext'
 import { CountdownContainer, Separator } from './styles'
 
 export function Countdown() {
-  const { activeCycle, activeCycleId, markCycleCurrentAsFinished } =
-    useContext(CycleContext)
-  const [amoutSecondsPassed, setAmountSecondsPassed] = useState(0)
+  const {
+    activeCycle,
+    activeCycleId,
+    markCycleCurrentAsFinished,
+    amountSecondsPassed,
+    setSecondsPassed,
+  } = useContext(CycleContext)
 
   const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0
 
@@ -18,26 +22,32 @@ export function Countdown() {
       interval = setInterval(() => {
         const secondsDifference = differenceInSeconds(
           new Date(),
-          activeCycle.startDate,
+          new Date(activeCycle.startDate),
         )
 
         if (secondsDifference >= totalSeconds) {
           markCycleCurrentAsFinished()
-          setAmountSecondsPassed(totalSeconds)
+          setSecondsPassed(totalSeconds)
           clearInterval(interval)
         } else {
-          setAmountSecondsPassed(secondsDifference)
+          setSecondsPassed(secondsDifference)
         }
       }, 1000)
     }
-  }, [activeCycle, activeCycleId, markCycleCurrentAsFinished, totalSeconds])
+  }, [
+    activeCycle,
+    activeCycleId,
+    markCycleCurrentAsFinished,
+    totalSeconds,
+    setSecondsPassed,
+  ])
 
-  const currentSeconds = activeCycle ? totalSeconds - amoutSecondsPassed : 0
+  const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0
 
-  const minutesAmout = Math.floor(currentSeconds / 60)
+  const minutesAmount = Math.floor(currentSeconds / 60)
   const secondsAmount = currentSeconds % 60
 
-  const minutes = String(minutesAmout).padStart(2, '0')
+  const minutes = String(minutesAmount).padStart(2, '0')
   const seconds = String(secondsAmount).padStart(2, '0')
 
   useEffect(() => {
